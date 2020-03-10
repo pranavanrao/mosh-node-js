52ec73d4c17661c8f25241303fadc6b5d5f84733
PRANAV N RAO <48349523+pranavanrao@users.noreply.github.com>
2020-03-10 09:27:49 +0530

    Add files via upload

---
+++ node pract/app.js
@@ -0,0 +1,21 @@
+// FIRST PRACTICE PROGRAM
+
+// function sayHello(name) {
+//     console.log(name);
+// }
+
+// // sayHello('mosh');
+// console.log(window);
+
+
+
+// GLOBAL OBJECT
+
+// var message="";
+// console.log(global.message);
+
+
+
+// MODULES
+
+console.log(module);
