7eda3b1b8c57027ce633aa6adc4c10427aec6f8c
PRANAV N RAO <48349523+pranavanrao@users.noreply.github.com>
2020-03-27 20:52:00 +0530

    Add files via upload

---
+++ node pract/modules/loading a module/app.js
@@ -0,0 +1,38 @@
+// var x=;
+// FIRST PRACTICE PROGRAM
+
+// function sayHello(name) {
+//     console.log(name);
+// }
+
+// // sayHello('mosh');
+// console.log(window);
+
+
+
+
+
+
+
+
+// GLOBAL OBJECT
+
+// var message="";
+// console.log(global.message);
+
+
+
+
+
+
+
+
+// MODULES
+
+// console.log(module);
+
+// creating a module and loading
+
+const log = require('./logger');
+log('message');
+//console.log(logger);
