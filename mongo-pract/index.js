801f2ea9f31f9367a30478e4fb7b83ccf9902718
PRANAV N RAO <48349523+pranavanrao@users.noreply.github.com>
2020-05-04 19:28:58 +0530

    Add files via upload

--- mongo-pract/index.js
+++ mongo-pract/index.js
@@ -25,4 +25,16 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+// createCourse();
+
+async function getCourse() {
+    const Course = mongoose.model('Course', courseSchema);
+    const courses = await Course
+        .find({ author: 'Mosh', isPublished: true })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1});
+    console.log(courses);
+}
+
+getCourse();
